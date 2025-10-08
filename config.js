5bcef4249094d3894f6ef1d5245b188d6d8a1173
DominicDev <s0l0estudi4r@gmail.com>
2025-10-08 18:25:28 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'IMAGINE DRAGONS';
+export let nameSongs = [
+	"BELIEVER", /*1*/
+	"DEMONS", /*2*/
+	"RADIOACTIVE", /*3*/
+	"BONES", /*4*/
+	"NATURAL", /*5*/
+	"FOLLOW YOU", /*6*/
+	"ZERO", /*7*/
+	"THUNDER", /*8*/
+	"WHATEVER IT TAKES", /*9*/
+	"BAD LIAR", /*10*/
+	"NICE TO MEET YOU", /*11*/
+	"EYES CLOSED ", /*12*/
+	"SHARKS", /*13*/
+	"LONELY", /*14*/
+	"WRECKED", /*15*/
+	"BIRDS", /*16*/
+	"ON TOP OF THE WORLD", /*17*/
+	"IT'S TIME", /*18*/
+	"WARRIORS", /*19*/
+	"I BET MY LIFE", /*20*/
+]
